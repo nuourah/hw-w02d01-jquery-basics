b9b198d6bf68959043a75b8421d4fc68539b9793
nuourah <49358431+nuourah@users.noreply.github.com>
2019-04-16 19:51:16 +0300

    submit

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-// turn on stop function 
-function turnOnStopLight() {
-    // change background color of stop light to red
-    // changes background color of green and yellow to black
+$('#stopButton').on('click',turnOnStopLight)
+function turnOnStopLight(){
+$('#stopLight').css('background-color','red');
+$('#slowLight').css('background-color','black');
+$('#goLight').css('background-color','black');
 }
 
-// turn on slow function
-function turnOnSlowLight() { 
-  // change background color of slow light to yellow
-  // changes background color of red and green to black
+$('#slowButton').on('click',turnOnSlowLight)
+function turnOnSlowLight(){
+$('#stopLight').css('background-color','black');
+$('#slowLight').css('background-color','yellow');
+$('#goLight').css('background-color','black');
 }
 
-// turn on go function
-function turnOnGoLight() {
-    // change background color of go light to green
-    // changes background color of red and yellow to black
+
+
+$('#goButton').on('click',turnOnGoLight)
+function turnOnGoLight(){
+$('#stopLight').css('background-color','black');
+$('#slowLight').css('background-color','black');
+$('#goLight').css('background-color','green');
 }
-
-// set stop light click event
-
-// set slow light click event
-
-// set go light click event
-
